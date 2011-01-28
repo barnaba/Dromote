dd344d538c17a1de821748efc7b83d4b53132092
Barnaba Turek <barnabaturek@gmail.com>
2011-01-28 01:23:22 +0100

    cleaning up the repo

--- dromote-server/dromote.py
+++ dromote-server/dromote.py
@@ -8,103 +8,133 @@ from mpris import *
 from dbus.mainloop.glib import DBusGMainLoop
 from synchronous_recipe import synchronous
 
+class RemoteUpdater(object):
 
-def signal_handler(signal, frame):
-  print 'Server terminating'
-  sys.exit(0)
+  def __init__(self, bus, lock):
+    self.lock = lock
+    self.bus = bus
 
-signal.signal(signal.SIGINT, signal_handler)
+    self.change = False
+    self.players = None
+    self.player = None
+    self.client = None
 
+  def update(self):
+    if not (self.client is None or self.player is None):
+      if (self.change):
+        status = self.__get_status_msg()
+        np = self.__get_now_playing_msg()
+        self.send(status, np)
+    self.change = False
 
-def handle_signal(interface_name, changed_properties, invalidated):
-  main.something_changed = True
+  def send(self, *args):
+    if not self.client is None:
+      for msg in args:
+        self.client.send(str(msg) + "\n")
 
-def update_now_playing():
-  if not (update_now_playing.player is None or update_now_playing.client is None):
-    player = update_now_playing.player
-    client = update_now_playing.client
-    msg = " ".join(('STATUS', str(int(player.volume()*100)), str(player.repeat_status()), str(player.shuffle_status())))
-    client.send(msg + "\n")
+  def update_players(self):
+    msg = self.__get_players_msg()
+    self.send(msg)
+
+  def __get_status_msg(self):
+    player = self.player
+    return " ".join(('STATUS', str(int(self.player.volume()*100)), \
+        str(self.player.repeat_status()), str(self.player.shuffle_status()))) 
+
+  def __get_now_playing_msg(self):
+    player = self.player
     if player.song_title() is None:
-      client.send("-\n")
+      return "-"
     else:
-      msg = '"' + player.song_title() + '" by ' + player.song_artist() + "\n"
-      client.send(msg)
+      return '"' + self.player.song_title() + '" by ' + self.player.song_artist()
+
+  def __get_players_msg(self):
+    if not self.players is None:
+      return "PLAYERS\n" + " ".join(self.players)
+
+  def set_player(self, i=0):
+    if not self.players is None:
+      self.player = MPRIS2Player(self.bus, self.players[i], self.lock)
+
+class DBusListener(object):
+
+  def __init__(self, lock):
+    self.lock = lock
+
+    gobject.threads_init()
+    DBusGMainLoop(set_as_default=True)
+
+    self.context = gobject.MainLoop().get_context()
+    self.bus = dbus.SessionBus()
+
+    def handle_signal(interface_name, changed_properties, invalidated):
+      self.updater.change = True
+
+    self.bus.add_signal_receiver(handle_signal, "PropertiesChanged", path="/org/mpris/MediaPlayer2")
+
+  def start(self):
+    updater = threading.Thread(target=self.runLoop)
+    updater.daemon = True
+    updater.start()
+
+  def runLoop(self):
+    while True:
+      with self.lock:
+        self.context.iteration(False)
+      self.updater.update()
+      time.sleep(0.1)
+
+class DromoteServer(object):
+  
+  def __init__(self, updater, hostname='', port=9006):
+    self.serversocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+    self.serversocket.bind((hostname, port))
+    self.serversocket.listen(5)
+    self.updater = updater
+
+  def accept(self):
+      (self.clientsocket, address) = self.serversocket.accept()
+      return self.clientsocket
+
+  def process_commands(self):
+      msg = self.clientsocket.recv(128).rstrip()
+      match = re.search("cycle (\d)",msg) 
+      if match:
+        self.updater.set_player(int(match.group(1)))
+        self.updater.change = True
+        self.updater.update
+      elif re.search("bye",msg):
+        self.clientsocket.sendall("bye\n")
+        self.updater.client = None
+        return False 
+      elif msg and hasattr(self.updater.player, msg):
+        fun = getattr(self.updater.player, msg)
+        fun()
+      return True 
 
 def main():  
-  main.something_changed = False
-  gobject.threads_init()
-  DBusGMainLoop(set_as_default=True)
-  context = gobject.MainLoop().get_context()
-
-  d = dbus.SessionBus()
-  d.add_signal_receiver(handle_signal, "PropertiesChanged", path="/org/mpris/MediaPlayer2")
-
-  lock = threading.Lock()
-  updater = threading.Thread(target=loopContext,args=(context,lock))
-  updater.daemon = True
-  updater.start()
-
-  serversocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
-  hostname = len(sys.argv) > 1 and sys.argv[1] or ''
-  serversocket.bind((hostname, 9006))
-  print "server started..."
-  serversocket.listen(5)
   try:
-    while 1:
-      i = 0
-      (clientsocket, address) = serversocket.accept()
-      update_now_playing.client = clientsocket
-      print "client connected: " , address
-      l = DbusPlayerList(d, lock)
-      if len(l.players) < 1:
-        print repr(l.players)
-        print "no mpris compliant players found :<"
-        print "dropping client!"
-        clientsocket.sendall("bye\n")
-        update_now_playing.client = None
-        time.sleep(1)
-        continue
-      player = MPRIS2Player(d, l.players[i], lock)
-      update_now_playing.player = player
-      update_now_playing()
-
-      clientsocket.sendall("PLAYERS\n" + " ".join(l.players) + "\n")
-      print "available players: " + " ".join(l.players)
-      print "connected player : " + str(l.players[i])
-
-      while clientsocket: 
-          msg = clientsocket.recv(128).rstrip()
-          m = re.search("cycle (\d)",msg) 
-          if m:
-            i = int(m.group(1))
-            player = MPRIS2Player(d, l.players[i], lock)
-            update_now_playing.player = player
-            update_now_playing()
-            print "connected player: " + str(l.players[i])
-          elif re.search("bye",msg):
-            clientsocket.sendall("bye\n")
-            update_now_playing.client = None
-            break
-          elif msg and hasattr(player, msg):
-              fun = getattr(player, msg)
-              fun()
-              print "command: " + msg
-      print "client disconnected"
+    lock = threading.Lock()
+    listener = DBusListener(lock)
+    updater = RemoteUpdater(listener.bus, lock)
+    listener.updater = updater
+    server = DromoteServer(updater)
+    listener.start()
+    while True:
+      client = server.accept()
+      updater.client = client
+      updater.players = DbusPlayerList(listener.bus, lock).players
+      updater.update_players()
+      updater.set_player()
+      while updater.client:
+        if not server.process_commands():
+          break
   except KeyboardInterrupt:
-    clientsocket.close()
-    serversocket.close()
+    #close sockets
+    pass
 
-def loopContext(context, lock):
-  while 1:
-    with lock:
-      context.iteration(False)
-    if main.something_changed:
-      update_now_playing()
-      main.something_changed = False
-    time.sleep(0.1)
+
 
 
 if __name__ == "__main__":
       main()
-
